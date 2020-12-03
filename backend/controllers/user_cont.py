8f76a0010a57a984f019bd45bc02dcb9b4572d1a
Lee Burgess <lee.j.burges77@gmail.com>
2020-12-03 15:00:32 +0000

    template socials

--- backend/controllers/user_cont.py
+++ backend/controllers/user_cont.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request
 from models.user_model import User
 from serializers.populate_user_schema import PopulateUserSchema
 from models.images_model import Images
+from models.socials_model import Socials
 
 user_schema = PopulateUserSchema()
 
@@ -13,7 +14,11 @@ def signup():
   template_images = Images(
     image1='https://res.cloudinary.com/spoondr/image/upload/v1607006914/Portrait_Placeholder_zu9uoa.png'
   )
+  template_social= Socials(
+    Instagram='https://www.instagram.com'
+  )
   user.images = [template_images]
+  user.socials = [template_social]
   user.save()
   return user_schema.jsonify(user), 200
 
