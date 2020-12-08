9c4b3f6b176d5c539d5832dc4477ce3a200b76f4
Harry Todd <harrytodd1997@gmail.com>
2020-12-08 14:24:02 +0000

    profile

--- frontend/components/UserProfile.js
+++ frontend/components/UserProfile.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import ReactDOM from 'react-dom'
+import { Carousel } from 'react-responsive-carousel'
 
 
 const Profile = (props) => {
   const userID = props.match.params.userID
 
   const [user, updateUser] = useState({})
+  let userImages = []
 
   useEffect(() => {
     axios.get(`/api/users/${userID}`)
       .then(res => {
-        updateUser(res.data)
-        console.log(res.data)
+        const resData = res.data
+        updateUser(resData)
+        userImages = Object.keys(resData.images[0]).map((key) => {
+          if (key.includes('image') && resData.images[0][key] !== null) {
+            userImages.push(resData.images[0][key])
+            console.log(userImages)
+          }
+        })
       })
   }, [])
 
@@ -19,7 +28,16 @@ const Profile = (props) => {
     return <h1>LOADING</h1>
   }
 
-  return <h1>hello</h1>
+  return <main>
+    <Carousel showArrows={true} swipeable={true} emulateTouch={true} showIndicators={true} infiniteLoop={true}>
+      {userImages.map((image, ind) => {
+        return <div key={ind}>
+          <img src={image} alt="image"/>
+          <p>Image 1</p>
+        </div>
+      })}
+    </Carousel>
+  </main>
 }
 
 export default Profile
