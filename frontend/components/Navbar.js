c7217cd46657a11aa3112c29828168c1f049b4b1
lauracharvey <lauracarolharvey@gmail.com>
2020-12-08 13:26:52 +0000

    lauraChanges

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -3,10 +3,20 @@ import { Link, withRouter } from 'react-router-dom'
 
 const Navbar = () => {
 
-  return <header>
+  return <footer>
     <nav>
+      <Link>
+      <img src='https://www.flaticon.com/svg/static/icons/svg/25/25379.svg' alt='link to messages'/>
+      </Link>
+      <Link>
+      <img src='https://www.flaticon.com/svg/static/icons/svg/25/25379.svg' alt='speech bubble'/>
+      </Link>
+      <Link>
+      <img src='https://www.flaticon.com/svg/static/icons/svg/2522/2522086.svg' alt='link to own profile'/>
+      </Link>
+    
     </nav>
-  </header>
+  </footer>
 
 }
 
