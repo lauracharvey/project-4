51d0bfc7787570816ab8e9fd99fe65af98b26451
Harry Todd <harrytodd1997@gmail.com>
2020-12-08 15:17:07 +0000

    profile attempt

--- frontend/components/UserProfile.js
+++ frontend/components/UserProfile.js
@@ -1,42 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import ReactDOM from 'react-dom'
-import { Carousel } from 'react-responsive-carousel'
+import Carousel from '@brainhubeu/react-carousel'
+import '@brainhubeu/react-carousel/lib/style.css'
 
 
 const Profile = (props) => {
   const userID = props.match.params.userID
-
   const [user, updateUser] = useState({})
-  let userImages = []
+  const [slideImages, updateSlideImages] = useState([])
 
   useEffect(() => {
     axios.get(`/api/users/${userID}`)
       .then(res => {
         const resData = res.data
         updateUser(resData)
-        userImages = Object.keys(resData.images[0]).map((key) => {
+        Object.keys(resData.images[0]).map((key) => {
           if (key.includes('image') && resData.images[0][key] !== null) {
-            userImages.push(resData.images[0][key])
-            console.log(userImages)
+            const images = [...slideImages, resData.images[0][key]]
+
+            updateSlideImages(images)
           }
         })
       })
   }, [])
 
-  if (!user.age) {
+  if (!slideImages[0]) {
     return <h1>LOADING</h1>
   }
 
   return <main>
-    <Carousel showArrows={true} swipeable={true} emulateTouch={true} showIndicators={true} infiniteLoop={true}>
-      {userImages.map((image, ind) => {
-        return <div key={ind}>
-          <img src={image} alt="image"/>
-          <p>Image 1</p>
-        </div>
-      })}
-    </Carousel>
+
   </main>
 }
 
