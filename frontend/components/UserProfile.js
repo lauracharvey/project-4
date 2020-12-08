070eac133742d6ca06bb3887fe1d1dea25e8bde9
Harry Todd <harrytodd1997@gmail.com>
2020-12-08 13:45:33 +0000

    .

--- frontend/components/UserProfile.js
+++ frontend/components/UserProfile.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 const Profile = (props) => {
+  const userID = props.match.params.userID
+
+  const [user, updateUser] = useState({})
+
+  useEffect(() => {
+    axios.get(`/api/users/${userID}`)
+      .then(res => {
+        updateUser(res.data)
+        console.log(res.data)
+      })
+  }, [])
+
+  if (!user.age) {
+    return <h1>LOADING</h1>
+  }
+
   return <h1>hello</h1>
 }
 
