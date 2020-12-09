77134b6c0efae217fd74df9a16cebcc2300db7ae
Harry Todd <harrytodd1997@gmail.com>
2020-12-09 14:59:02 +0000

    looking for settings done

--- frontend/components/Settings.js
+++ frontend/components/Settings.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { getUserId } from '../lib/UserToken'
 import Header from './Header'
@@ -7,43 +8,41 @@ import Footer from './Navbar'
 
 const Settings = () => {
   const userID = getUserId()
+  const [currUser, updateCurrUser] = useState({})
 
-  function handleSelect(e) {
-    console.log(e.target.id)
+  useEffect(() => {
+    axios.get(`/api/users/${userID}`)
+      .then(res => {
+        updateCurrUser(res.data)
+        console.log(res.data)
+      })
+  }, [])
+
+  function handleGenderPref(event) {
+    const value = event.target.value
+    const data = {
+      ...currUser,
+      gender_preference: value
+    }
+    updateCurrUser(data)
+    console.log(data)
   }
 
+
+
   return <main className="settingsMain">
-    <Header/>
+    <Header />
     <form>
-      <select name='genderpreference' defaultChecked={'Male'}>
-        <label>Looking for:
-          <input 
-            type="radio"
-            id="Male"
-            name="gender"
-            onFocus={handleSelect}
-        />
-        <label htmlFor="male">Male</label>
-
-        <input 
-          type="radio"
-          id="Female"
-          name="gender"
-          onFocus={handleSelect}
-        />
-        <label htmlFor="female">Female</label>
-
-        <input 
-          type="radio"
-          id="Other"
-          name="gender"
-          onFocus={handleSelect}
-        />
-        <label htmlFor="other">Other</label>
+      <label>Looking for
+        <select name="gender_preference" onChange={handleGenderPref}>
+          <option disabled selected>Please Select</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          <option value="Both">Both</option>
+        </select>
       </label>
-      </select>
     </form>
-    <Footer/>
+    <Footer />
   </main>
 }
 
