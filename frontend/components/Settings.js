ec7a7a2d231f08c0c91ac1a3a945fd6cf344c8f6
lauracharvey <lauracarolharvey@gmail.com>
2020-12-09 15:04:08 +0000

    adding logout

--- frontend/components/Settings.js
+++ frontend/components/Settings.js
@@ -1,49 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { getUserId } from '../lib/UserToken'
-import Header from './Header'
 import Footer from './Navbar'
+import Back from '../images/previous.png'
 
 
-const Settings = () => {
+const Settings = (props) => {
   const userID = getUserId()
+
+  function handleLogout() {
+    localStorage.removeItem('token')
+    props.history.push('/')
+  }
 
   function handleSelect(e) {
     console.log(e.target.id)
   }
 
   return <main className="settingsMain">
-    <Header/>
+    <header>
+      <div>
+        <img src={Back} alt="back"/>
+      </div>
+      <div>
+        <h1>Spoondr.</h1>
+      </div>
+      <div>
+        {localStorage.getItem('token')
+          && <img onClick={handleLogout} src={'https://www.flaticon.com/svg/static/icons/svg/1828/1828427.svg'} alt={'signout'}/>}
+      </div>
+    </header>
+
     <form>
-      <select name='genderpreference' defaultChecked={'Male'}>
-        <label>Looking for:
-          <input 
-            type="radio"
-            id="Male"
-            name="gender"
-            onFocus={handleSelect}
-        />
-        <label htmlFor="male">Male</label>
-
-        <input 
-          type="radio"
-          id="Female"
-          name="gender"
-          onFocus={handleSelect}
-        />
-        <label htmlFor="female">Female</label>
-
-        <input 
-          type="radio"
-          id="Other"
-          name="gender"
-          onFocus={handleSelect}
-        />
-        <label htmlFor="other">Other</label>
+      <label>Looking for:
+        <select name="gender" onChange={handleSelect}>
+          <option>Male</option>
+          <option>Female</option>
+          <option>Both</option>
+        </select>
       </label>
-      </select>
     </form>
-    <Footer/>
+    <Footer />
   </main>
 }
 
