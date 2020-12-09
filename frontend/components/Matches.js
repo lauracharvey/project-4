a918ced10de9504b4794ac65815585ea1df89666
Lee Burgess <lee.j.burges77@gmail.com>
2020-12-09 10:14:19 +0000

    fix

--- frontend/components/Matches.js
+++ frontend/components/Matches.js
@@ -15,7 +15,7 @@ const Matches = (props) => {
       headers: { Authorization: `Bearer ${token}` }
     })
       .then(resp => {
- 
+
         updateMatches(resp.data.Matched)
       })
 
@@ -37,46 +37,30 @@ const Matches = (props) => {
     return filter.filter(user => user !== undefined)
   }
 
-<<<<<<< HEAD
-
-  return <div>
-    <h1>Matches</h1>
-    {filterMatched().map((user, index) => {
-      const userChats = user.chats.filter((chat) => {
-        return chat.user1 === 3 || chat.user2 === 3
-      })
-
-      return <div key={index} style={{ border: '1px solid black', width: 'fit-content' }}>
-        <img src={user.images[0].image1} style={{ width: '75px', height: 'auto' }}></img>
-        <h2>{user.first_name}</h2>
-        <p>{user.bio}</p>
-        <p>User Chat ID = {userChats[0].id}</p>
-=======
   console.log(filterMatched())
   return <main className="matchesMain">
     <div className="chatContainer">
 
     </div>
-    <Header/>
+    <Header />
     <h2>your matches - tap to chat</h2>
     <div className="matchesContainer">
       {filterMatched().map((user, index) => {
-      const userChats = user.chats.filter((chat) => {
-        return chat.user1 === 3 || chat.user2 === 3
-      })
-      console.log(userChats[0].id)
-      return <div key={index}>
->>>>>>> development
-        <Link to={`/matches/chat/${userChats[0].id}`}>
-          <div className="userImage">
-          <img src={user.images[0].image1}/>
-          </div>
-        </Link>
-      </div>
-    })}
+        const userChats = user.chats.filter((chat) => {
+          return chat.user1 === 3 || chat.user2 === 3
+        })
+        console.log(userChats[0].id)
+        return <div key={index}>
+          <Link to={`/matches/chat/${userChats[0].id}`}>
+            <div className="userImage">
+              <img src={user.images[0].image1} />
+            </div>
+          </Link>
+        </div>
+      })}
     </div>
-    
-    <Footer/>
+
+    <Footer />
   </main>
 
 
